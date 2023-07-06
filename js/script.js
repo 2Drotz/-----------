ecc6a1a3eb21a1c11f7e5c612426a0ef4a91fbac
= <=>
2023-07-06 15:17:37 +0300

    gamberger menu update

--- js/script.js
+++ js/script.js
@@ -2,11 +2,9 @@
 
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.nav-main');
-
 menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
-
-
 })
 
 body.onload = function () {
