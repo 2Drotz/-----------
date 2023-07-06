d28885a5042acc6401c5e04a0eec33c24184afdb
= <=>
2023-07-06 14:47:19 +0300

    change style and add gamburger menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.nav-main');
+
+menuBtn.addEventListener('click', function(){
+	menu.classList.toggle('active');
+
+
+})
+
 body.onload = function () {
    let preloader = document.getElementById('preloader');
    preloader.style.display = "none";
