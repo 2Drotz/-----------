2666ea4d88aa27ad2b0eb67370c53a6431e1db8d
= <=>
2023-07-06 16:42:15 +0300

    update burger menu

--- js/script.js
+++ js/script.js
@@ -5,9 +5,15 @@ let menu = document.querySelector('.nav-main');
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
+   if(menu.classList.contains('active')){
+      disableScroll.on();
+   }else {
+      disableScroll.off();
+   }
 })
 
 body.onload = function () {
    let preloader = document.getElementById('preloader');
    preloader.style.display = "none";
 }
+
