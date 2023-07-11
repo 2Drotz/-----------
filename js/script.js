5afc43f8cfd22ca016fc3015a6f74758d270a3b6
= <=>
2023-07-11 16:02:33 +0300

    dsf

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 let body = document.querySelector('body');
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.nav-main');
+
+
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
@@ -12,4 +14,3 @@ body.onload = function () {
    let preloader = document.getElementById('preloader');
    preloader.style.display = "none";
 }
-
